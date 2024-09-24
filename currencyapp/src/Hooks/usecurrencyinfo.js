b883241b349a68a450098cebb8b881523261c9c0
Thatrajpatil <rajerajpatil197@gmail.com>
2024-09-24 16:04:02 +0530

    commit

--- currencyapp/src/Hooks/usecurrencyinfo.js
+++ currencyapp/src/Hooks/usecurrencyinfo.js
@@ -1,5 +1,11 @@
 import { useEffect , useState } from 'react' 
 
-function useCurrencyinfo(currency){() => {
-
-} , []}
+function useCurrencyinfo(currency){
+    const {Data , setData} = useState({})
+    useEffect(()=>{
+        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        .then((res) => res.json)
+        .then((res) => setData(res[currency]))
+        console.log(Data)
+    } , [currency])
+}
