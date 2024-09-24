4f1d9d63837e360645f5953a0ef8f0abc9962935
Thatrajpatil <rajerajpatil197@gmail.com>
2024-09-24 15:55:11 +0530

    commit

--- currencyapp/src/App.jsx
+++ currencyapp/src/App.jsx
@@ -2,13 +2,11 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-       <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    
     </>
   )
 }
---
+++ currencyapp/src/Hooks/usecurrencyinfo.js
@@ -0,0 +1,5 @@
+import { useEffect , useState } from 'react' 
+
+function useCurrencyinfo(currency){() => {
+
+} , []}
