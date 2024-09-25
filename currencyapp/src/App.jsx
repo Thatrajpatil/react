9367f0d0cf58ef6ade12aaf73ae2d3527c177fca
Thatrajpatil <rajerajpatil197@gmail.com>
2024-09-25 06:56:33 +0530

    commit

--- currencyapp/src/App.jsx
+++ currencyapp/src/App.jsx
@@ -1,8 +1,26 @@
 import { useState } from 'react'
+import { InputBox } from './components'
+import useCurrencyinfo from './Hooks/usecurrencyinfo'
 import './App.css'
 
 function App() {
- 
+ const [amount , setAmount] = useState(0)
+ const [from ,setFrom] = useState("usd")
+ const [to , setto ] = useState("inr")
+ const [convertedAmount , setconvertedAmount] = useState(0)
+
+ const Currencyinfo = useCurrencyinfo(from)
+ const options = Object.keys(Currencyinfo)
+
+
+ const swap = ()=> {
+   setFrom(to)
+   setto(from)
+   setconvertedAmount(setAmount)
+   setAmount(convertedAmount)
+
+ }
+
 
   return (
     <>
