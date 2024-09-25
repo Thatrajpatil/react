36befc98b7199dddeead2586349ae8febbe234bc
Thatrajpatil <rajerajpatil197@gmail.com>
2024-09-25 08:08:02 +0530

    commit

--- currencyapp/src/App.jsx
+++ currencyapp/src/App.jsx
@@ -76,7 +76,7 @@ function App() {
                       />
                   </div>
                   <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
-                      Convert {from.toUpperCase}  to  {to.toUpperCase}
+                      Convert {from.toUpperCase()}  to  {to.toUpperCase()}
                   </button>
               </form>
           </div>
