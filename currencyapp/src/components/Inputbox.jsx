0fd75117cb3bda7d7dc13257c9080577b05065e9
Thatrajpatil <rajerajpatil197@gmail.com>
2024-09-25 06:03:31 +0530

    commit

--- currencyapp/src/components/Inputbox.jsx
+++ currencyapp/src/components/Inputbox.jsx
@@ -5,7 +5,7 @@ function InputBox({
     amount,
     onAmountChange,
     onCurrencyChange,
-    currencyoption = [],
+    currencyoptions = [],
     selectCurrency = "usd",
     amountDisable = false,
     currencyDisable = false,    
@@ -33,13 +33,20 @@ function InputBox({
                 <p className="text-black/40 mb-2 w-full">Currency Type</p>
                 <select
                     className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+                    value={selectCurrency}
+                    onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
+                    disabled={currencyDisable}
+
                     
                 >
-                    
-                        <option value="usd">
-                            usd
-                        </option>
-                
+                    {currencyoptions.map((currency) => (
+                        <option key={currency} value={currency}>
+                        {currency}
+                    </option>
+            
+                    ))}
+
+                        
                 </select>
             </div>
         </div>
